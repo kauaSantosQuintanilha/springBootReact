ba01d594e78f4a73aeca2adc2487638f889deb6e
kaua_quintanilha <kaua_santos_quintanilha@hotmail.com>
2025-04-14 21:04:49 -0300

    Initial commit

---
+++ src/main/java/com/springbootreact/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.springbootreact.repository;
+
+import com.springbootreact.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
